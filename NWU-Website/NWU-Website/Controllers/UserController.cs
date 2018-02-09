a3e3d08e7616587d0e36b860dae2e2b8d8db605b
sayig86 <sayig86@gmail.com>
2018-02-09 13:28:49 +0100

    PersonaleOprettelse
    Slettet gamle controller oprettet nyt

--- NWU-Website/NWU-Website/Controllers/UserController.cs
+++ NWU-Website/NWU-Website/Controllers/UserController.cs
@@ -1,127 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using NWU_Website.Models;
+using NWU_Website.ViewModel;
 
 namespace NWU_Website.Controllers
 {
     public class UserController : Controller
     {
-        private nwuDBEntities db = new nwuDBEntities();
-
         // GET: User
         public ActionResult Index()
         {
-            return View(db.personales.ToList());
-        }
-
-        // GET: User/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            personale personale = db.personales.Find(id);
-            if (personale == null)
-            {
-                return HttpNotFound();
-            }
-            return View(personale);
-        }
-
-        // GET: User/Create
-        public ActionResult Create()
-        {
+            UserViewModel userViewModel = new UserViewModel();
+            userViewModel.personale = new personale();
             return View();
         }
 
-        // POST: User/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "personaleID,fornavn,efternavn,brugernavn,password,rolleID")] personale personale)
+       
+        public ActionResult CreateUser()
         {
-            if (ModelState.IsValid)
-            {
-                db.personales.Add(personale);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(personale);
-        }
-
-        // GET: User/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            personale personale = db.personales.Find(id);
-            if (personale == null)
-            {
-                return HttpNotFound();
-            }
-            return View(personale);
-        }
-
-        // POST: User/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "personaleID,fornavn,efternavn,brugernavn,password,rolleID")] personale personale)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(personale).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(personale);
-        }
-
-        // GET: User/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            personale personale = db.personales.Find(id);
-            if (personale == null)
-            {
-                return HttpNotFound();
-            }
-            return View(personale);
-        }
-
-        // POST: User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            personale personale = db.personales.Find(id);
-            db.personales.Remove(personale);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return View();
         }
     }
 }
---
+++ NWU-Website/NWU-Website/ViewModel/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NWU_Website.Models;
+
+namespace NWU_Website.ViewModel
+{
+    public class UserViewModel
+    {
+        public personale personale
+        {
+            get; set;
+        }
+
+    }
+}
