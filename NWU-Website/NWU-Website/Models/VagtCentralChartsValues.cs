2796cfd8daf74165d850fb0fb21d9ffeb9a7aa7f
ClausOlsen72 <30793350+ClausOlsen72@users.noreply.github.com>
2018-02-12 13:11:21 +0100

    Vagtcentral GUI færdig
    mangler at lave database connection ned til tabeler på serveren

---
+++ NWU-Website/NWU-Website/Models/VagtCentralChartsValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWU_Website.Models
+{
+    // Hold values from database
+    // For testing purpose only
+    // Replace later with real tabel on server - Claus
+    public class VagtCentralChartsValues
+    {
+        public int Tal1 { get; set; }
+        public int Tal2 { get; set; }
+        public int Tal3 { get; set; }
+        public int Tal4 { get; set; }
+        public int Tal5 { get; set; }
+        public int Tal6 { get; set; }
+        public int Tal7 { get; set; }
+        public int Tal8 { get; set; }
+    }
+}
