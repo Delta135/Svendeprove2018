b2ca1f0670ef32744a32d78f87868103b3a050c3
sayig86 <sayig86@gmail.com>
2018-02-13 12:10:10 +0100

    error fix

--- NWU-Website/NWU-Website/Controllers/VagtCentralController.cs
+++ NWU-Website/NWU-Website/Controllers/VagtCentralController.cs
@@ -20,23 +20,23 @@ namespace NWU_Website.Controllers
             // The using block close and run dispose after - Claus 
             // TalTabelContext er forbindelsen på Claus's computer til lokal DB
            
-            using (var context = new TalTabelContext())
-            {
-                // LINQ statement who find and put all values in the row where 
-                // id is 1 into the var q - Claus
-                var q = context.TalTabels.Where(s => s.id == 1);
+            //using (var context = new TalTabelContext())
+            //{
+            //    // LINQ statement who find and put all values in the row where 
+            //    // id is 1 into the var q - Claus
+            //    var q = context.TalTabels.Where(s => s.id == 1);
 
-                // Convert LINQ query result into object values - Claus 
-                vcObj.Tal1 = (int)q.First().Tal1;
-                vcObj.Tal2 = (int)q.First().Tal2;
-                vcObj.Tal3 = (int)q.First().Tal3;
-                vcObj.Tal4 = (int)q.First().Tal4;
-                vcObj.Tal5 = (int)q.First().Tal5;
-                vcObj.Tal6 = (int)q.First().Tal6;
-                vcObj.Tal7 = (int)q.First().Tal7;
-                vcObj.Tal8 = (int)q.First().Tal8;
+            //    // Convert LINQ query result into object values - Claus 
+            //    vcObj.Tal1 = (int)q.First().Tal1;
+            //    vcObj.Tal2 = (int)q.First().Tal2;
+            //    vcObj.Tal3 = (int)q.First().Tal3;
+            //    vcObj.Tal4 = (int)q.First().Tal4;
+            //    vcObj.Tal5 = (int)q.First().Tal5;
+            //    vcObj.Tal6 = (int)q.First().Tal6;
+            //    vcObj.Tal7 = (int)q.First().Tal7;
+            //    vcObj.Tal8 = (int)q.First().Tal8;
 
-            }
+            //}
             // Pause the method for 5 seconds - Claus
             Thread.Sleep(5000);
 
