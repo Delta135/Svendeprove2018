90391463e1f36e21e2d0e2e339e3fd1c94a29f38
Delta135 <135.delta@gmail.com>
2018-02-16 08:49:30 +0100

    Revert "History GUI Færdig"

--- NWU-Website/NWU-Website/Controllers/VagtCentralController.cs
+++ NWU-Website/NWU-Website/Controllers/VagtCentralController.cs
@@ -55,43 +55,8 @@ namespace NWU_Website.Controllers
 
         public ActionResult RunningDemo()
         {
-            // Showing what our program will look like in real life
+
             return View();
-        }
-
-        public ActionResult ShowHistoryData(int? area, string dato)
-        {
-            
-            HistoryData ttc = new HistoryData();
-            nwuDB1Entities3  db = new nwuDB1Entities3();
-           
-            // if area is assigned a value
-            if (area.HasValue)
-            {
-                // put all data from the table checkIND where kortlaeserID is the same as area into test
-                var test = db.checkINDs.Where(o => o.kortlaeserID == area);
-
-                // sort after date and time
-                test = test.OrderBy(o => o.ckeckIND);
-
-                // we transform a var result to a list<checkIND> result
-                List<checkIND> testRedultater = test.ToList<checkIND>();
-
-
-                ttc.Tal1 = (int)testRedultater.ElementAt(0).antalPersoner; //first result
-                ttc.Tal2 = (int)testRedultater.ElementAt(1).antalPersoner;
-                ttc.Tal3 = (int)testRedultater.ElementAt(2).antalPersoner;
-                ttc.Tal4 = (int)testRedultater.ElementAt(3).antalPersoner;
-                ttc.Tal5 = (int)testRedultater.ElementAt(4).antalPersoner;
-                ttc.Tal6 = (int)testRedultater.ElementAt(5).antalPersoner;
-                ttc.Tal7 = (int)testRedultater.ElementAt(6).antalPersoner;
-                ttc.Tal8 = (int)testRedultater.ElementAt(7).antalPersoner;
-                ttc.Tal9 = (int)testRedultater.ElementAt(8).antalPersoner;
-                ttc.Tal10 = (int)testRedultater.ElementAt(9).antalPersoner;
-                ttc.Tal11 = (int)testRedultater.ElementAt(10).antalPersoner;
-            }
-
-            return View(ttc);            
         }
     }
 }
